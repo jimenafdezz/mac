18aa29dff40ea5a5a9a4a48e3a0cd20ed0ecab29
jimenafdezz <jimena.fernandezlabrador@iesmiguelherrero.com>
2024-06-17 15:08:52 +0200

    cucos

---
+++ src/main/java/com/mycompany/cucu/Cucu.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.cucu;
+
+/**
+ *
+ * @author DAW107
+ */
+public class Cucu {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
