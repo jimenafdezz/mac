24c1552dfbab23e8e365e2cce5f1c6b511ad65ea
jimenafdezz <153528628+jimenafdezz@users.noreply.github.com>
2024-06-17 15:27:30 +0200

    Update Cucu.java

--- src/main/java/com/mycompany/cucu/Cucu.java
+++ src/main/java/com/mycompany/cucu/Cucu.java
@@ -11,6 +11,6 @@ package com.mycompany.cucu;
 public class Cucu {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("la vida es una tombola");
     }
 }
